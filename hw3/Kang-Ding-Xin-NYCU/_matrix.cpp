8c337e3e18c4dccbb3ebb97d13229dcfc5fdb6c4
Kang-Ding-Xin-NYCU <kang313552040.cs13@nycu.edu.tw>
2024-10-23 18:00:04 +0800

    change matrix.cpp

--- hw3/Kang-Ding-Xin-NYCU/_matrix.cpp
+++ hw3/Kang-Ding-Xin-NYCU/_matrix.cpp
@@ -36,9 +36,9 @@ public:
         {
             reset_buffer(other.m_nrow, other.m_ncol);
         }
-        for (size_t i=0; i<m_nrow; ++i)
+        for (size_t i = 0; i < m_nrow; ++i)
         {
-            for (size_t j=0; j<m_ncol; ++j)
+            for (size_t j = 0; j < m_ncol; ++j)
             {
                 (*this)(i,j) = other(i,j);
             }
@@ -46,7 +46,20 @@ public:
         return *this;
     }
 
-    // TODO: move constructors and assignment operators.
+    bool operator==(Matrix const & other)
+    {
+        if ((this->m_nrow != other.nrow()) || (this->m_ncol != other.ncol())){
+            return false;
+        }
+        for(int i = 0; i < other.ncol(); i++){
+            for(int j = 0; j < other.nrow(); j++){
+                if(this->m_buffer[index(i, j)] != other(i, j)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
     ~Matrix()
     {
@@ -66,42 +79,25 @@ public:
     size_t ncol() const { return m_ncol; }
 
     size_t size() const { return m_nrow * m_ncol; }
-    double buffer(size_t i) const { return m_buffer[i]; }
-    std::vector<double> buffer_vector() const
-    {
-        return std::vector<double>(m_buffer, m_buffer+size());
-    }
-
-    bool is_transposed() const { return m_transpose; }
-
-    Matrix & transpose()
-    {
-        m_transpose = !m_transpose;
-        std::swap(m_nrow, m_ncol);
-        return *this;
-    }
 
 private:
 
     size_t index(size_t row, size_t col) const
     {
-        if (m_transpose) { return row          + col * m_nrow; }
-        else             { return row * m_ncol + col         ; }
+        return row * m_ncol + col;
     }
 
     void reset_buffer(size_t nrow, size_t ncol)
     {
         if (m_buffer) { delete[] m_buffer; }
         const size_t nelement = nrow * ncol;
-        if (nelement) { m_buffer = new double[nelement]; }
+        if (nelement) { m_buffer = new double[nelement] = {0}; }
         else          { m_buffer = nullptr; }
         m_nrow = nrow;
         m_ncol = ncol;
     }
-
     size_t m_nrow = 0;
     size_t m_ncol = 0;
-    bool m_transpose = false;
     double * m_buffer = nullptr;
 };
 
@@ -110,11 +106,11 @@ Matrix multiply_naive(Matrix const & m1, Matrix const & m2){
         throw std::invalid_argument("wrong dimention");
     }
     Matrix res(m1.nrow(), m2.ncol());
-    for(int i = 0; i < m1.nrow; ++i){
+    for(int i = 0; i < m1.nrow(); ++i){
         for(int j = 0; j < m2.ncol(); ++j){
             double element = 0;
-            for(int k = 0; k < m1.ncol; ++k){
-                element += m1(i, k)*m2(k, j);
+            for(int k = 0; k < m1.ncol(); ++k){
+                element += m1(i, k) * m2(k, j);
             }
             res(i, j) = element;
         }
@@ -122,20 +118,25 @@ Matrix multiply_naive(Matrix const & m1, Matrix const & m2){
     return res;
 }
 
-Matrix multiply_tile(Matrix const & m1, Matrix const & m2){
+Matrix multiply_tile(Matrix const & m1, Matrix const & m2, size_t t){
     if(m1.ncol()!=m2.nrow()){
         throw std::invalid_argument("wrong dimention");
     }
     Matrix res(m1.nrow(), m2.ncol());
-    for(int i = 0; i < m1.nrow; ++i){
-        for(int j = 0; j < m2.ncol(); ++j){
-            double element = 0;
-            for(int k = 0; k < m1.ncol; ++k){
-                element += m1(i, k)*m2(k, j);
+    for(int i = 0; i < m1.nrow(); i += t){
+        for(int j = 0; j < m2.ncol(); j += t){
+            for(int k = 0; k < m1.ncol(); k += t){
+                for(int it = i; it < std::min(m1.nrow(), i + t); ++it){
+                    for(int jt = j; jt < std::min(m2.ncol(), j + t); ++jt){
+                        for(int kt = k; kt < std::min(m1.ncol(), k + t); ++kt){
+                            res(it, jt) += m1(it, kt) * m2(kt, jt);
+                        }
+                    }
+                }
             }
-            res(i, j) = element;
         }
     }
+
     return res;
 }
 
@@ -150,4 +151,21 @@ Matrix multiply_mkl(Matrix const & m1, Matrix const & m2){
     cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
                 m, n, k, 1.0, m1, k, m2, n, 1.0, res, n);
     return res;
+}
+
+PYBIND11_MODULE(_matrix, m){
+    m.def("multiply_naive", &multiply_naive, "use naive");
+    m.def("multiply_tile", &multiply_tile, "use tile");
+    m.def("multiply_mkl", &multiply_mkl, "use mkl");
+    pybind11::class_<Matrix>(m, "Matrix")
+        .def(pybind11::init<size_t, size_t>())
+        .def("__setitem__", [](Matrix &self, std::pair<size_t, size_t> i, double val) {
+            self(i.first, i.second) = val;
+        })
+        .def("__getitem__", [](Matrix &self, std::pair<size_t, size_t> i) {
+            return self(i.first, i.second);
+        })
+        .def("__eq__", &Matrix::operator==)
+        .def_property("nrow", &Matrix::nrow, nullptr)
+        .def_property("ncol", &Matrix::ncol, nullptr);
 }
